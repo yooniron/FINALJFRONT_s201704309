554ffda109bd3aa3db7db9c251516116d958bbc6
yooniron <dbscjf324@gmail.com>
2021-12-20 13:09:38 +0900

    기말고사

---
+++ README.md
@@ -0,0 +1,10 @@
+## Installation
+
+### **Project install**
+
+```
+git clone https://github.com/yooniron/test-vue-final.git 
+npm install
+npm run dev
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+
+<v-app>
+    <app-bar/>
+
+        <v-content class="white">
+            <router-view/>
+        </v-content>
+</v-app>
+</template>
+
+<script>
+import AppBar from "./components/AppBar";
+import Modal from "./common/Modal.vue"
+export default {
+  name: 'App',
+
+  components: {
+    AppBar,
+    Modal
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
